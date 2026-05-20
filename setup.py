7d768bdc07ca8c34a3e51c23af984c59ce1b8121
Thomas Uher <thomas.uher@code-for-nature.com>
2026-05-20 17:07:06 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import find_packages, setup
 
 setup(
 	name='django-eyesonly',
-	version='0.1.0',
+	version='0.1.2',
 	description='Eyes Only Django application',
 	packages=find_packages(include=['eyesonly', 'eyesonly.*']),
+	include_package_data=True,
 	install_requires=[
 		'djangorestframework>=3.15,<4',
 		'djangorestframework-simplejwt>=5.3,<6',
-		'cryptography>=46,<47',
+		'cryptography>=48',
 		'PyYAML>=6,<7',
 		'uritemplate>=4,<5',
 		'inflection>=0.5,<1',
